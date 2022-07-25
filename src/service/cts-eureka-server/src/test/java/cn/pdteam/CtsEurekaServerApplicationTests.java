41de493f32b4e141b856d43b6beefe9bb0abef38
Armando <l.dzh@163.com>
2022-07-26 01:07:52 +0800

    Initial commit

---
+++ src/service/cts-eureka-server/src/test/java/cn/pdteam/CtsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.pdteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtsEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
